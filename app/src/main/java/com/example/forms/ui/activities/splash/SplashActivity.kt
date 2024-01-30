7df5c3463ced7e723150348b9516646528e25390
Andry R Santos <anrodriguessantos0@gmail.com>
2024-01-30 18:38:56 -0300

    first commit

--- app/src/main/java/com/example/forms/ui/activities/splash/SplashActivity.kt
+++ app/src/main/java/com/example/forms/ui/activities/splash/SplashActivity.kt
@@ -1,10 +1,15 @@
 package com.example.forms.ui.activities.splash
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.forms.R
+import android.os.Handler
+import android.os.Looper
 import com.example.forms.databinding.ActivitySplashBinding
+import com.example.forms.ui.activities.login.LoginActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivitySplashBinding
@@ -13,5 +18,10 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
